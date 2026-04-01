40c56ef9c611a4983b3bdbec7082ca2a620b6380
Long CV <cvlongvn@gmail.com>
2026-04-01 16:55:29 +0700

    feature: greate

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('Hello, world! This is the entry point of the application.');
 console.log('Hello, nestjs.');
 console.log('Demo.');
 console.log('main: great');
+console.log('feature: great');
